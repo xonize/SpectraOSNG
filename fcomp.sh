e766269c186dc5f1a5be30baae4326283d88d30f
Westsi <jamie@rockyroost.com>
2023-09-14 18:30:05 +0100

    updates

--- fcomp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Full Compilation and Run - clean, config, build, iso, qemu
-set -e
-. ./clean.sh
-. ./config.sh
-. ./build.sh
-. ./iso.sh
-. ./qemu.sh
--- qemu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-. ./iso.sh
-
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom spectraos.iso -d int -D qemu_log.txt -M smm=off
