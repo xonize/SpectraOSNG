8d82098338774a9702e726255b0be27836d6a1bb
Westsi <jamie@rockyroost.com>
2023-09-18 20:37:19 +0100

    works! lets go!

--- kernel/tty.c
+++ kernel/tty.c
@@ -12,13 +12,13 @@ static size_t ATTRIBUTE = 7;
 static uint32_t PITCH; // number of bytes that are in each row on screen
 static uint8_t FBTYPE; // color type/mode of framebuffer
 static uint32_t s_color; // default color
-static volatile void* fb;
+static volatile char* fb;
 
 int calculate_offset(int x, int y);
 
 
 void screen_init(void* addr, uint32_t width, uint32_t height, uint32_t pitch, uint8_t bpp, uint8_t colormode) {
-    fb = addr;
+    fb = (char*) addr;
     WIDTH = width;
     HEIGHT = height;
     PITCH = pitch;
@@ -29,7 +29,7 @@ void screen_init(void* addr, uint32_t width, uint32_t height, uint32_t pitch, ui
 
 
 void screen_draw_img(char* data, uint32_t x, uint32_t y) {
-    char pixel[4];
+    uint32_t pixel[4];
     for (uint32_t yy=0;yy<logo_height;yy++) {
         for (uint32_t xx=0;xx<logo_width;xx++) {
             HEADER_PIXEL(data, pixel);
@@ -38,8 +38,8 @@ void screen_draw_img(char* data, uint32_t x, uint32_t y) {
             // Num 
             uint32_t num = (uint32_t) pixel[0] << 16 |
                 (uint32_t)pixel[1] << 8 |
-                (uint32_t)pixel[2]  |
-                (uint32_t)pixel[3];
+                (uint32_t)pixel[2];
+                // (uint32_t)pixel[3];
             screen_put_pixel(x+xx, y+yy, num);
         }
     }
@@ -47,7 +47,7 @@ void screen_draw_img(char* data, uint32_t x, uint32_t y) {
 
 void screen_put_pixel(uint32_t x, uint32_t y, uint32_t color) {
     uint32_t* pixel = fb + calculate_offset(x, y);
-    *pixel = (color & 0xffffff) | (*pixel & 0xff000000);
+    *pixel = color;
 }
 
 int calculate_offset(int x, int y) {
